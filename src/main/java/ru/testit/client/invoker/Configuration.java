a22e5a6057368d5bcfb4a0852cddfe81427458cd
Dmitry Ermakovich <dmitry.ermakovich@testit.software>
2025-06-24 19:05:07 +0300

    fix: TMS-33599 customattributemodel fix

--- src/main/java/ru/testit/client/invoker/Configuration.java
+++ src/main/java/ru/testit/client/invoker/Configuration.java
@@ -15,7 +15,7 @@ package ru.testit.client.invoker;
 
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", comments = "Generator version: 7.9.0")
 public class Configuration {
-    public static final String VERSION = "0.4.5";
+    public static final String VERSION = "1.0.1-TMS-5.2.2";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
